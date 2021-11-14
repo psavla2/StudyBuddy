54779a6bc079993c75cb7744f4bf6ad86fa96c25
SethWyma <wyma2@illinois.edu>
2021-11-14 14:13:59 -0600

    Added first 10 study locations

--- app/src/main/java/edu/illinois/cs465/studybuddy/StudySpace.java
+++ app/src/main/java/edu/illinois/cs465/studybuddy/StudySpace.java
@@ -6,22 +6,24 @@ import java.util.Set;
 
 public class StudySpace {
     int id;
+    String maps_id;
     String name;
+    String short_name;
     String description;
-    double latitude;
-    double longitude;
     Set<Integer> tags;
+    String website;
 
     @NonNull
     @Override
     public String toString() {
         return "StudySpace{" +
                 "id=" + id +
+                ", maps_id='" + maps_id + '\'' +
                 ", name='" + name + '\'' +
+                ", short_name='" + short_name + '\'' +
                 ", description='" + description + '\'' +
-                ", latitude=" + latitude +
-                ", longitude=" + longitude +
                 ", tags=" + tags +
+                ", website='" + website + '\'' +
                 '}';
     }
 }
