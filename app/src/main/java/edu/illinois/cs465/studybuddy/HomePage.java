5a506a4a9d820f2ed3bfeab50f410685b60221c0
foladeji <folade2@illinois.edu>
2021-11-16 19:42:03 -0600

    Updates to homepage
    1. Adjusted/changes card icons.
    2. Included vertical scroll for tags on card
    
    To do:
    Array of playlist cards similar to array of tags on filter search page.

--- app/src/main/java/edu/illinois/cs465/studybuddy/HomePage.java
+++ app/src/main/java/edu/illinois/cs465/studybuddy/HomePage.java
@@ -5,10 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 
+import com.wajahatkarim3.easyflipview.EasyFlipView;
+
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
 
-import java.util.HashSet;
 
 public class HomePage extends AppCompatActivity {
 
@@ -16,16 +17,16 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page2);
-
-        ChipGroup filters = findViewById(R.id.onboarding_chips);
+        ChipGroup filters = findViewById(R.id.homepage_chips);
         Tag[] tags = JsonReader.getTags(this);
         LayoutInflater inflater = LayoutInflater.from(this);
 
         for (Tag t : tags) {
-            Chip chip = (Chip)inflater.inflate(R.layout.space_filter_chip, filters, false);
+            Chip chip = (Chip)inflater.inflate(R.layout.homepage_tags, filters, false);
             chip.setId(t.id);
             chip.setText(t.tag);
             filters.addView(chip);
         }
     }
+
 }
